0e44d08ad233277c7d80dc431ed64b07be3edaf5
iSliderJS <opanlol@mail.ru>
2023-08-04 21:02:17 +0300

    Add sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,15 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import house from '../icon/house.svg';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { useState } from 'react';
+import sidebar from '../icon/sidebar.svg';
 
 function Header({ changeTheme, theme }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <Navbar className="head bg-warning bg-gradient">
       <Container>
@@ -12,12 +19,33 @@ function Header({ changeTheme, theme }) {
           <span className="ms-2 logoText">TTK House</span>
         </Navbar.Brand>
         <Nav className="ms-auto me-2 nav-header">
-          <Nav.Link href="#home">Главная</Nav.Link>{' '}
+          <Nav.Link href="#home"></Nav.Link>{' '}
         </Nav>
-        <label id="switch" className="switch">
-          <input type="checkbox" onChange={changeTheme} id="slider" />
-          <span class="slider round"></span>
-        </label>
+        <img
+          alt="sidebar"
+          src={sidebar}
+          width="40"
+          height="40"
+          onClick={handleShow}
+          className="me-2"
+        />
+        <Offcanvas
+          data-bs-theme={theme}
+          placement="end"
+          show={show}
+          onHide={handleClose}
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Меню</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            Смена темы:{' '}
+            <label id="switch" className="switch">
+              <input type="checkbox" onChange={changeTheme} id="slider" />
+              <span class="slider round"></span>
+            </label>
+          </Offcanvas.Body>
+        </Offcanvas>
       </Container>
     </Navbar>
   );
