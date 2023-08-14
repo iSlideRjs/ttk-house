bd33dc64d9386d81f80df956add54d92226915d4
iSliderJS <opanlol@mail.ru>
2023-08-14 20:56:33 +0300

    Add social link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,44 @@
+import Card from 'react-bootstrap/Card';
+import Nav from 'react-bootstrap/Nav';
+
 function Footer() {
   return (
-    <div className="block2 d-flex justify-content-center align-items-end">
-      <span className="mb-2">TTK House {new Date().getFullYear()}</span>
+    <div className="block2 d-flex justify-content-center align-items-center">
+      <Card border="warning" style={{ width: '15rem' }}>
+        <Card.Header>Связь с нами</Card.Header>
+        <Card.Body>
+          <Card.Text>
+            {' '}
+            <Nav className="flex-column">
+              <a
+                className="social"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://vk.com"
+              >
+                VKontakte
+              </a>
+              <a
+                className="social"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://vk.com"
+              >
+                Telegram
+              </a>
+              <a
+                className="social"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://vk.com"
+              >
+                Instagram
+              </a>
+            </Nav>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      {/* <span className="mb-2">TTK House {new Date().getFullYear()}</span> */}
     </div>
   );
 }
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,24 +2,24 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function Slider() {
   return (
-    <Carousel className="mt-4 ms-2 me-2 mb-4">
+    <Carousel className="mt-4 ms-2 me-2 mb-4 picture">
       <Carousel.Item>
         <img
-          className="d-block w-100 rounded-2"
+          className="d-block w-100"
           src="https://langformula.ru/wp-content/uploads/2019/04/house.jpg"
           alt="First slide"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100 rounded-2"
+          className="d-block w-100"
           src="https://langformula.ru/wp-content/uploads/2019/04/house.jpg"
           alt="Second slide"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100 rounded-2"
+          className="d-block w-100"
           src="https://langformula.ru/wp-content/uploads/2019/04/house.jpg"
           alt="Third slide"
         />
