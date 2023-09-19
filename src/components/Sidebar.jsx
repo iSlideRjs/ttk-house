e48bcd43abf98c57658d8f1ff9b32c64aab4a8c7
iSliderJS <opanlol@mail.ru>
2023-09-19 20:00:57 +0300

    Edit contacts in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,6 +5,9 @@ import Nav from 'react-bootstrap/Nav';
 import insta from '../icon/insta.svg';
 import teleg from '../icon/teleg.svg';
 import vk from '../icon/vk.svg';
+import phone2 from '../icon/phone2.svg';
+import mail from '../icon/mail.svg';
+import avito from '../icon/avito.png';
 
 function Sidebar({ changeTheme, theme, show, handleClose, handleTouchMove }) {
   return (
@@ -25,9 +28,9 @@ function Sidebar({ changeTheme, theme, show, handleClose, handleTouchMove }) {
       </Offcanvas.Header>
       <Offcanvas.Body>
         <ListGroup className="list-group-flush">
-          <ListGroup.Item className="border-top">
+          <ListGroup.Item className="border-top d-flex justify-content-between">
             {' '}
-            <Nav className="flex-column mb-3 mt-2">
+            <Nav className="mb-3 mt-2">
               <a
                 className={classNames('social', {
                   'social-light': theme === 'light',
@@ -63,6 +66,43 @@ function Sidebar({ changeTheme, theme, show, handleClose, handleTouchMove }) {
               >
                 <img src={insta} alt="phone" width="20px" height="20px" />{' '}
                 Instagram
+              </a>
+            </Nav>
+            <Nav className="mb-3 mt-2">
+              <a
+                className={classNames('social', {
+                  'social-light': theme === 'light',
+                  'social-dark': theme === 'dark',
+                })}
+                target="_blank"
+                rel="noopener noreferrer"
+                href="tel:+79510827471"
+              >
+                <img src={phone2} alt="phone" width="20px" height="20px" />{' '}
+                +7-951-082-74-71
+              </a>
+              <a
+                className={classNames('social', {
+                  'social-light': theme === 'light',
+                  'social-dark': theme === 'dark',
+                })}
+                target="_blank"
+                rel="noopener noreferrer"
+                href="mailto:ttk@ttk.ru"
+              >
+                <img src={mail} alt="phone" width="20px" height="20px" />{' '}
+                ttk@ttk.ru
+              </a>
+              <a
+                className={classNames('social', {
+                  'social-light': theme === 'light',
+                  'social-dark': theme === 'dark',
+                })}
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://avito.ru"
+              >
+                <img src={avito} alt="phone" width="20px" height="20px" /> Avito
               </a>{' '}
             </Nav>{' '}
           </ListGroup.Item>
